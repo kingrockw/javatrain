f71c33a5f193afe095f8acbf2efee06497ac9201
wjl48511 <wjl48511@ly.com>
2019-06-11 20:27:45 +0800

    注解解析

---
+++ model2demo/src/main/java/cn/rock/business/controller/Test.java
@@ -0,0 +1,9 @@
+package cn.rock.business.controller;
+
+/**
+ *
+ * @author wjl48511
+ * @create 2019/6/11-20:10
+ **/
+public class Test {
+}
--- model2demo/src/main/java/cn/rock/frame12/ApplicationMvcContext.java
+++ model2demo/src/main/java/cn/rock/frame12/ApplicationMvcContext.java
@@ -22,11 +22,11 @@ public class ApplicationMvcContext {
 
         // 是否循环迭代
         boolean recursive = true;
-        Set<Class<?>> classes = new LinkedHashSet<Class<?>>();
+        Set<Class<?>> classes = new LinkedHashSet<>();
         String packageName = controllerPath;
         String packageDirName = packageName.replace('.', '/');
 
-        Enumeration<URL> dirs = ApplicationMvcContext.class.getClassLoader().getResources(controllerPath);
+        Enumeration<URL> dirs = ApplicationMvcContext.class.getClassLoader().getResources(packageDirName);
 
         while (dirs.hasMoreElements()) {
             // 获取下一个元素
@@ -38,9 +38,34 @@ public class ApplicationMvcContext {
                 // 获取包的物理路径
                 String filePath = URLDecoder.decode(url.getFile(), "UTF-8");
                 // 以文件的方式扫描整个包下的文件 并添加到集合中
-                findAndAddClassesInPackageByFile(packageName, filePath,
-                        recursive, classes);
+                findAndAddClassesInPackageByFile(packageName, filePath, recursive, classes);
             }
+        }
+        parseMvcDefine(classes,mvcContext);
+    }
+
+    private static void parseMvcDefine(Set<Class<?>> classes, Map<String, MvcDefine> mvcContext) {
+        for (Class<?> item : classes){
+            Object o = null;
+            try {
+                o = item.newInstance();
+            } catch (InstantiationException e) {
+
+            } catch (IllegalAccessException e) {
+
+            }
+            if (o != null){
+                Method[] methods = item.getMethods();
+                for(Method method : methods){
+                    RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                    if (null != annotation){
+                        String path = annotation.value();
+                        MvcDefine mvcDefine = new MvcDefine(path,method,o);
+                        mvcContext.put(path,mvcDefine);
+                    }
+                }
+            }
+
         }
     }
 
@@ -52,9 +77,7 @@ public class ApplicationMvcContext {
      * @param recursive
      * @param classes 文件列表
      */
-    public static void findAndAddClassesInPackageByFile(String packageName,
-                                                        String packagePath,
-                                                        final boolean recursive,
+    public static void findAndAddClassesInPackageByFile(String packageName, String packagePath, final boolean recursive,
                                                         Set<Class<?>> classes) {
         // 获取此包的目录 建立一个File
         File dir = new File(packagePath);
@@ -65,41 +88,53 @@ public class ApplicationMvcContext {
         // 如果存在 就获取包下的所有文件 包括目录
         File[] dirfiles = dir.listFiles(new FileFilter() {
             // 自定义过滤规则 如果可以循环(包含子目录) 或则是以.class结尾的文件(编译好的java类文件)
+            @Override
             public boolean accept(File file) {
-                return (recursive && file.isDirectory())
-                        || (file.getName().endsWith(".class"));
+                return (recursive && file.isDirectory()) || (file.getName().endsWith(".class"));
             }
         });
         // 循环所有文件
         for (File file : dirfiles) {
             // 如果是目录 则继续扫描
             if (file.isDirectory()) {
-                findAndAddClassesInPackageByFile(packageName + "."
-                                + file.getName(), file.getAbsolutePath(), recursive,
+                findAndAddClassesInPackageByFile(packageName + "." + file.getName(), file.getAbsolutePath(), recursive,
                         classes);
             } else {
                 // 如果是java类文件 去掉后面的.class 只留下类名
-                String className = file.getName().substring(0,
-                        file.getName().length() - 6);
+                String className = file.getName().substring(0, file.getName().length() - 6);
                 try {
-                    // 添加到集合中去
                     //classes.add(Class.forName(packageName + '.' + className));
                     //经过回复同学的提醒，这里用forName有一些不好，会触发static方法，没有使用classLoader的load干净
-                    classes.add(Thread.currentThread().getContextClassLoader().loadClass(packageName + '.' + className));
+                    Class<?> clazz = Thread.currentThread().getContextClassLoader()
+                            .loadClass(packageName + '.' + className);
+
+                    Controller annotation = clazz.getAnnotation(Controller.class);
+                    if (null != annotation){
+                        classes.add(clazz);
+                    }
                 } catch (ClassNotFoundException e) {
-                    // log.error("添加用户自定义视图类错误 找不到此类的.class文件");
-                    e.printStackTrace();
+
                 }
             }
         }
     }
 
-    public class MvcDefine {
+    public static void main(String[] args) throws IOException {
+        init("cn.rock.business.controller");
+    }
+
+    public static class MvcDefine {
         private String path;
 
         private Method method;
 
         private Object target;
+
+        public MvcDefine(String path, Method method, Object target) {
+            this.path = path;
+            this.method = method;
+            this.target = target;
+        }
 
         public String getPath() {
             return path;
@@ -124,9 +159,5 @@ public class ApplicationMvcContext {
         public void setTarget(Object target) {
             this.target = target;
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        init("cn.rock.business.controller");
     }
 }
